59c991c201e6bc7f851f270020ec5a4e6e6d37bc
iTrailytics <marinersdrive.com@gmail.com>
2024-03-25 15:41:06 +0530

    final changes

--- src/OnBoardTraining.js
+++ src/OnBoardTraining.js
@@ -222,7 +222,7 @@ function OnBoardTraining() {
             </p>
           </div>
         </div>
-        <div className="bg-white lg:h-56 h-48 p-4 flex items-center">
+        <div className="bg-white lg:h-56 h-48 lg:p-4 flex items-center">
           <p className="text-black text-sm font-montserrat items-center text-justify leading-6 font-medium">
           Click here to get detailed information about {news.heading} and the latest updates essential for your On-Board training. Prepare thoroughly with our resources for a successful outcome.
 
