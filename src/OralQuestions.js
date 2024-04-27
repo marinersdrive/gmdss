1367794e6ccf5c69039cfcc427bb3764954ace47
MarinersDrive <marinersdrive.com@gmail.com>
2024-04-28 00:02:13 +0530

    final changes

--- src/OralQuestions.js
+++ src/OralQuestions.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import oralQues from './oralQues.json';
 import Logo from "./assets/bg2.png";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBackward, faForward } from "@fortawesome/free-solid-svg-icons";
 import { Document, Page } from 'react-pdf';
@@ -52,9 +52,8 @@ function OralQuestions() {
 
 
   return (
-    <div className="lg:top-0 lg:left-0 lg:right-0 lg:bg-gradient-to-t from-darkest-blue to-black2 lg:z-50 lg:py-6 py-16 lg:px-0 px-6 overflow-y-auto">
+    <div className="lg:top-0 lg:left-0 lg:right-0 lg:bg-gradient-to-t from-darkest-blue to-black2 lg:z-50 lg:py-6 py-8 lg:px-0 px-6 pb-16 overflow-y-auto">
       <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center bg-white lg:px-16 py-6 px-12 rounded-lg max-w-4xl mx-auto md:mt-20 lg:mt-0 lg:mb-0 font-montserrat shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] overflow-y-auto relative">
           <div className="mb-4">
             <img
               src={Logo}
@@ -65,12 +64,12 @@ function OralQuestions() {
           <FontAwesomeIcon icon={faBackward} 
             style={{"--fa-primary-color": "#143261", "--fa-secondary-color": "#143261",}}         
             size="2xs"
-            className="text-blue-900 text-2xl lg:text-4xl cursor-pointer absolute top-1/2 lg:left-0 left-3 lg:mt-24 lg:ml-3"
+            className="text-blue-800 text-2xl lg:text-8xl cursor-pointer absolute top-1/2 lg:left-24 left-4 lg:ml-3"
             onClick={handlePrevSet}
             />
           <div className="text-center mb-6">
-            <h2 className="text-xl lg:text-2xl font-semibold mt-0 text-darkest-blue font-montserrat tracking-normal">{captName}</h2>
-            {filteredData && filteredData.length > 0 && (<h2 className="text-xl lg:text-2xl font-semibold mt-0 text-darkest-blue font-montserrat tracking-normal">(Set {filteredData[currentSetIndex].set})</h2>)}
+            <h2 className="text-xl lg:text-2xl font-semibold mt-0 text-gray-300 font-montserrat tracking-normal">{captName}</h2>
+            {filteredData && filteredData.length > 0 && (<h2 className="text-xl lg:text-2xl font-semibold mt-0 text-gray-300 font-montserrat tracking-normal">(Set {filteredData[currentSetIndex].set})</h2>)}
           </div>
           {filteredData && filteredData.length > 0 && (
             <div className="mb-4">
@@ -80,13 +79,13 @@ function OralQuestions() {
                 seamless=""
                 width={700}
                 height={600}
-                className="block lg:hidden w-64 h-96"
+                className="block lg:hidden w-96 h-96"
                 sandbox="allow-scripts allow-same-origin"
               />
             </div>
           )}
           {filteredData && filteredData.length > 0 && (
-            <div className="mb-4">
+            <div className="lg:mb-4 mb-0">
               <iframe
                 title="PDF Viewer"
                 src={filteredData[currentSetIndex].url}
@@ -100,13 +99,14 @@ function OralQuestions() {
           )}
           <FontAwesomeIcon
           icon={faForward}
-          className="text-blue-900 text-2xl lg:text-4xl cursor-pointer absolute top-1/2 lg:right-0 right-3 lg:mt-24 lg:mr-3"
+          className="text-blue-800 text-2xl lg:text-8xl cursor-pointer absolute top-1/2 lg:right-24 right-4 lg:mr-3"
           onClick={handleNextSet}
         />
+        <Link to="/" className="text-white text-center ml-2 border-b-2 sm:text-base text-xs transition duration-300 ease-in-out transform hover:scale-105 relative">˂ Back ˃</Link>
+
         </div>
         
       </div>
-    </div>
   );
 }
 
